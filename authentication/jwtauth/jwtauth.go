f4fc393ea6a4fb2d2011dbd7ccfa59f3e972afbb
Gufran Mirza <gufranmirza1@gmail.com>
2020-11-03 22:46:16 +0530

    UPdate interface

--- authentication/jwtauth/jwtauth.go
+++ authentication/jwtauth/jwtauth.go
@@ -1,24 +1,30 @@
 package jwtauth
 
 import (
+	"time"
+
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
 type jwtAuth struct {
-	signKey   interface{}
-	verifyKey interface{}
-	signer    jwt.SigningMethod
-	parser    *jwt.Parser
+	signKey          interface{}
+	verifyKey        interface{}
+	signer           jwt.SigningMethod
+	parser           *jwt.Parser
+	jwtExpiry        time.Duration
+	jwtRefreshExpiry time.Duration
 }
 
 // NewJWTAuth creates a JWTAuth authenticator instance that provides middleware handlers
 // and encoding/decoding functions for JWT signing.
 // *jwt.Parser is custom parser settings introduced in jwt-go/v2.4.0.
-func NewJWTAuth(alg string, parser *jwt.Parser, signKey interface{}, verifyKey interface{}) JWTAuth {
+func NewJWTAuth(config Config) JWTAuth {
 	return &jwtAuth{
-		signKey:   signKey,
-		verifyKey: verifyKey,
-		signer:    jwt.GetSigningMethod(alg),
-		parser:    parser,
+		signKey:          config.SignKey,
+		verifyKey:        config.VerifyKey,
+		signer:           jwt.GetSigningMethod(config.JwtAuthAlgo),
+		parser:           config.JwtParser,
+		jwtExpiry:        config.JwtExpiry,
+		jwtRefreshExpiry: config.JwtRefreshExpiry,
 	}
 }
